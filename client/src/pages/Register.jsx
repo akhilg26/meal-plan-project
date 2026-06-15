a33d46a686b5165ceecdb1eb0a5222e1f6c49e1b
akhilg26 <akhil.giddaluri@gmail.com>
2026-06-15 11:35:49 -0400

    .

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { register } from '../api/auth'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 function Register(){
     const [username, setUsername] = useState('')
